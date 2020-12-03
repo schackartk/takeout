4cc65a4bc97cb50987f94d05072b74a872e6e329
Kenneth Schackart <schackartk1@gmail.com>
2020-12-02 17:45:42 -0700

    add some tests for properly functioning json_to_tibble

--- tests/testthat/test-json_to_tibble.R
+++ tests/testthat/test-json_to_tibble.R
@@ -1,3 +1,13 @@
 test_that("good error on bad file", {
   expect_error(json_to_tibble("foo.json"),regexp = "'foo.json' does not*")
 })
+
+test_that("works with good location history file", {
+  expect_equal(ncol(json_to_tibble("sample_location_history.json")), 4)
+  expect_equal(nrow(json_to_tibble("sample_location_history.json")), 3)
+})
+
+test_that("works with good local guides file", {
+  #expect_equal(ncol(json_to_tibble("sample_ratings_reviews.json")), 4)
+  #expect_equal(nrow(json_to_tibble("sample_ratings_revies.json")), 3)
+})
